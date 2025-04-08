1076a24027314c1d49d89aed154e11a5e2cf8712
samuelaguiar99 <sam.silva@live.com.pt>
2025-04-08 19:12:17 +0100

    Added doc in nvim-springboot declaration to fix creation of java class in correct directory

--- lua/plugins/springboot-nvim.lua
+++ lua/plugins/springboot-nvim.lua
@@ -50,4 +50,59 @@ local function find_main_application_class_directory(root_path)
     end
 end
 
+
+
+
+
+Also change function generate_class in \home\samuel\.local\share\nvim\lazy\springboot-nvim\lua\springboot-nvim\generate_class.lua
+so that it handles the creation of the class in the correct directory
+
+local function generate_class()
+    local file_path = vim.fn.fnamemodify(start_buf, ':p')
+    local path_pattern = "(.-)/java"
+    local root_path = file_path:match(path_pattern) .. "/java"
+    -- Make sure there is content in the class buffer
+    local class_lines = api.nvim_buf_get_lines(bufs[4], 0, -1, false)
+    local class_content = table.concat(class_lines)
+    local package_lines = api.nvim_buf_get_lines(bufs[3], 0, -1, false)
+    local base_package_path = table.concat(package_lines)
+    local package_path = base_package_path:gsub("%.", "/")
+    if (package_path:sub(-1, -1) ~= '/' and package_path ~= '') then
+        package_path = package_path .. "/"
+    end
+
+    if(class_content ~= '') then
+        -- Check the specified package directory to make sure it exists
+        if(vim.fn.isdirectory(root_path .. '/' .. package_path) == 1) then
+            print("package directory exists")
+        else
+            -- Make the package directory if it does not exist
+            local command = 'mkdir -p ' .. root_path .. '/' .. package_path
+			local quoted_command = '"' .. command .. '"'
+            os.execute(quoted_command)
+        end
+        -- Generate the new java file and inject boiler plate
+        -- Need to strip and trailing periods from the package
+        if(base_package_path:sub(-1, -1) == '.') then
+            base_package_path = string.sub(base_package_path, 1, -2)
+        end
+        local java_file_content = string.format(class_boiler_plate, base_package_path, class_content)
+        local java_file = io.open(root_path .. "/" .. package_path .. class_content .. ".java", "r")
+        if(java_file) then
+            print("Class already exists in package")
+            java_file:close()
+        else
+            java_file = io.open(root_path .. "/" .. package_path .. class_content .. ".java", "w")
+            java_file:write(java_file_content)
+            java_file:close()
+            close_generate_class()
+            local path = root_path .. "/" .. package_path .. class_content .. ".java"
+            vim.cmd('edit ' .. vim.fn.fnameescape(path))
+        end
+    else
+        print("Please specify a class name to continue")
+    end
+
+end
+
 ]]--
