b0f7ff32aacbf4b86ba3fb8a83e46faa85f4d272
samuelaguiar99 <sam.silva@live.com.pt>
2025-06-09 00:03:10 +0100

    Replace minipairs for nvim-autopairs & add Folke's todo-comments

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -1,16 +1,14 @@
-return 
-{ 
+return
+{
 	'echasnovski/mini.nvim',
 	version = false ,
 	config = function()
-		require("mini.pairs").setup({})
+		-- require("mini.pairs").setup({})
 		require("mini.animate").setup({
 			open = { enable = false },
 			close = { enable = false },
 		})
 		require("mini.sessions").setup({})
 	end
-	
-	
 }
 
