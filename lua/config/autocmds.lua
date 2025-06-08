973e822f0dcb5763f4197646c1692770d1cc2690
samuelaguiar99 <sam.silva@live.com.pt>
2025-06-08 21:25:19 +0100

    Setup autocmds for auto save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local general = augroup("General", { clear = true })
+
+
 -- Setup our JDTLS server any time we open up a java file
 vim.cmd [[
     augroup jdtls_lsp
@@ -5,3 +10,25 @@ vim.cmd [[
         autocmd FileType java lua require'config.jdtls'.setup_jdtls()
     augroup end
 ]]
+
+
+--Disable readonly file message when saving automatically
+vim.api.nvim_create_autocmd("FileChangedRO", {
+  callback = function()
+    vim.bo.readonly = false
+  end,
+  group = general,
+  desc = "Disable Readonly Message",
+})
+
+--Auto save
+autocmd({ "FocusLost", "BufLeave", "BufWinLeave", "InsertLeave" }, {
+  -- nested = true, -- for format on save
+  callback = function()
+    if vim.bo.filetype ~= "" and vim.bo.buftype == "" then
+      vim.cmd "silent! w"
+    end
+  end,
+  group = general,
+  desc = "Auto Save",
+})
