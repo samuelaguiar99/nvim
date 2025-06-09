543d9e0aa06d0b2bc3da54280b17a2d68e1c81bd
samuelaguiar99 <sam.silva@live.com.pt>
2025-06-09 15:14:05 +0100

    Added mini-trailspace

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -58,5 +58,25 @@ return
             silent = false,
         },
     },
+	{
+        "echasnovski/mini.trailspace",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            local miniTrailspace = require("mini.trailspace")
+
+            miniTrailspace.setup({
+                only_in_normal_buffers = true,
+            })
+            vim.keymap.set("n", "<leader>cw", function() miniTrailspace.trim() end, { desc = "Erase Whitespace" })
+
+            -- Ensure highlight never reappears by removing it on CursorMoved
+            vim.api.nvim_create_autocmd("CursorMoved", {
+                pattern = "*",
+                callback = function()
+                    require("mini.trailspace").unhighlight()
+                end,
+            })
+        end,
+    },
 }
 
