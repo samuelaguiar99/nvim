cfabd7dfa7ff02f6a3d3ad0dd020d8ef60141ce0
samuelaguiar99 <sam.silva@live.com.pt>
2025-06-10 20:45:31 +0100

    Changed noice-nvim config & added snacks picker for neovim conf folder

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,14 +2,17 @@ return {
 	{
 		"folke/snacks.nvim",
 		priority = 1000,
-		lazy = false , 
+		lazy = false ,
 		opts = {},
 		keys = {
 			{ "<leader>lg", function() require("snacks").lazygit() end, desc = "Lazygit" },
             { "<leader>lgl", function() require("snacks").lazygit.log() end, desc = "Lazygit Logs" },
             { "<leader>rN", function() require("snacks").rename.rename_file() end, desc = "Fast Rename Current File" },
             { "<leader>dB", function() require("snacks").bufdelete() end, desc = "Delete or Close Buffer  (Confirm)" },
+
+            --Snacks picker
+            {"<leader>vc" , function() require("snacks").picker.files ( { cwd = vim.fn.stdpath("config")}) end , desc = "Find Neovim Config Files"}
 		}
 	},
-	
+
 }
