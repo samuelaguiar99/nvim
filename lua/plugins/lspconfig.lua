eb5d922a49d182be2f952fcfc3a5d1bb2d94daeb
samuelaguiar99 <sam.silva@live.com.pt>
2025-07-12 18:16:18 +0100

    Changed diagnostics icons & more things

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,6 +2,8 @@ return
 {
         "neovim/nvim-lspconfig",
         config = function()
+
+
             -- get access to the lspconfig plugins functions
             local lspconfig = require("lspconfig")
 
@@ -99,12 +101,33 @@ return
 			vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float, { desc = "[S]how [D]iagnostic" })
 
 
+
+
+            -- Set custom diagnostic icons
+            local signs = {
+              [vim.diagnostic.severity.ERROR] = " ", 
+              [vim.diagnostic.severity.WARN]  = " ",
+              [vim.diagnostic.severity.HINT] =  " ",
+              [vim.diagnostic.severity.INFO] = " ",
+            }
+
             vim.diagnostic.config({
-                virtual_text = {
-                    severity = vim.diagnostic.severity.ERROR,
+                virtual_text = false ,
+                signs = {
+                    active = true ,
+                    text = signs,
+                } ,
+                underline = {
+                    severity = {
+                        min = vim.diagnostic.severity.HINT,
+                        max = vim.diagnostic.severity.HINT,
+                    }
                 },
-                underline = true ,
                 update_in_insert = false ,
+                severity_sort = true , 
             })
+            vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "#555555" })
+
+
         end
     }
