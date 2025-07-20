328fe02d70c1c4e32fea88fa66c4a450641217a6
samuelaguiar99 <sam.silva@live.com.pt>
2025-07-20 22:45:21 +0100

    Change : Config blankline plugin

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,7 +1,14 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = { char = "│" },
+    scope = {
+      enabled = true,
+      show_start = false,
+      show_end = false,
+      injected_languages = false,
+      highlight = { "Normal" },  -- ✅ Use built-in group with no underline
+    },
+  },
 }
